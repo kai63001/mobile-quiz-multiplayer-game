0c97862bf5db837675481b0a652fd83666dfdbc0
kai63001 <animehq5@gmail.com>
2021-09-05 12:52:03 +0700

    update

--- server/src/main.ts
+++
@@ -1,9 +0,0 @@
-import express, { Application, Request, Response } from 'express'
-
-const app: Application = express()
-
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!')
-})
-
-app.listen(3000)
